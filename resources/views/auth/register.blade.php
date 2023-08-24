13c40faa83f6738737016867024491b7f3c222f2
Magiksen <elemejore@gmail.com>
2023-08-24 13:25:34 -0400

    Register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,109 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+<!doctype html>
+<html lang="en">
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+<head>
+
+    <meta charset="utf-8" />
+    <title>Register | Admin</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Laravel 10 admin template" name="description" />
+    <meta content="Magiksen" name="author" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="{{ asset('backend/assets/images/favicon.ico') }}">
+
+    <!-- Bootstrap Css -->
+    <link href="{{ asset('backend/assets/css/bootstrap.min.css') }}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="{{ asset('backend/assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="{{ asset('backend/assets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body class="auth-body-bg">
+<div class="bg-overlay"></div>
+<div class="wrapper-page">
+    <div class="container-fluid p-0">
+        <div class="card">
+            <div class="card-body">
+
+                <div class="text-center mt-4">
+                    <div class="mb-3">
+                        <a href="index.html" class="auth-logo">
+                            <img src="{{ asset('backend/assets/images/logo-dark.png') }}" height="30" class="logo-dark mx-auto" alt="">
+                            <img src="{{ asset('backend/assets/images/logo-light.png') }}" height="30" class="logo-light mx-auto" alt="">
+                        </a>
+                    </div>
+                </div>
+
+                <h4 class="text-muted text-center font-size-18"><b>Register</b></h4>
+
+                <div class="p-3">
+                    <form class="form-horizontal mt-3" method="POST" action="{{ route('register') }}">
+                        @csrf
+
+                        <div class="form-group mb-3 row">
+                            <div class="col-12">
+                                <input class="form-control" id="name" name="name" :value="old('name')" type="text" required autofocus placeholder="Nombre">
+                            </div>
+                        </div>
+
+                        <div class="form-group mb-3 row">
+                            <div class="col-12">
+                                <input class="form-control" id="username" name="username" :value="old('username')" type="text" required autofocus placeholder="Usuario">
+                            </div>
+                        </div>
+
+                        <div class="form-group mb-3 row">
+                            <div class="col-12">
+                                <input class="form-control" id="email" name="email" :value="old('email')"  type="email" required placeholder="Email">
+                            </div>
+                        </div>
+
+                        <div class="form-group mb-3 row">
+                            <div class="col-12">
+                                <input class="form-control" id="password" name="password" type="password" required placeholder="Contraseña" autocomplete="new-password">
+                            </div>
+                        </div>
+
+                        <div class="form-group mb-3 row">
+                            <div class="col-12">
+                                <input class="form-control" id="password_confirmation" name="password_confirmation" type="password" required placeholder="Repetir contraseña">
+                            </div>
+                        </div>
+
+                        <div class="form-group text-center row mt-3 pt-1">
+                            <div class="col-12">
+                                <button class="btn btn-info w-100 waves-effect waves-light" type="submit">Registrar</button>
+                            </div>
+                        </div>
+
+                        <div class="form-group mt-2 mb-0 row">
+                            <div class="col-12 mt-3 text-center">
+                                <a href="{{ route('login') }}" class="text-muted">Ya tienes una cuenta?</a>
+                            </div>
+                        </div>
+                    </form>
+                    <!-- end form -->
+                </div>
+            </div>
+            <!-- end cardbody -->
         </div>
+        <!-- end card -->
+    </div>
+    <!-- end container -->
+</div>
+<!-- end -->
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+<!-- JAVASCRIPT -->
+<script src="{{ asset('backend/assets/libs/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('backend/assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('backend/assets/libs/metismenu/metisMenu.min.js') }}"></script>
+<script src="{{ asset('backend/assets/libs/simplebar/simplebar.min.js') }}"></script>
+<script src="{{ asset('backend/assets/libs/node-waves/waves.min.js') }}"></script>
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
+<script src="{{ asset('backend/assets/js/app.js') }}"></script>
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ml-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+</body>
+</html>
